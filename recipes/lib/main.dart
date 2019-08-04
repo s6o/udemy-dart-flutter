472ea04efa7807dc50898528fe9cd3708f61f177
Oliver Sõro <oliver.s6ro@gmail.com>
2019-08-04 21:20:36 +0300

    Set up custom fonts; dummy data; categories screen. #recipes

--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './ui/categories_screen.dart';
+
 void main() => runApp(RecipesApp());
 
 class RecipesApp extends StatelessWidget {
@@ -9,32 +11,25 @@ class RecipesApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Recipes',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
-      home: _RecipesScreen(title: 'Recipes'),
-    );
-  }
-}
-
-class _RecipesScreen extends StatefulWidget {
-  _RecipesScreen({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _RecipesState createState() => _RecipesState();
-}
-
-class _RecipesState extends State<_RecipesScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text('Recipes Screen'),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
---
+++ recipes/lib/ui/categories_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:recepies/models/dummy_data.dart';
+
+import '../models/category.dart';
+import './category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recipes: Categories'),
+      ),
+      body: _categoryGrid(),
+    );
+  }
+
+  Widget _categoryGrid() {
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map((Category c) => CategoryItem(c.title, c.color))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
