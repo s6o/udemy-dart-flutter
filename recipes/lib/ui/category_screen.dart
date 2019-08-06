27b7d81f30ebd77fb156a8cd3c36c6c6581b2408
Oliver Sõro <oliver.s6ro@gmail.com>
2019-08-06 08:44:59 +0300

    Add dummy data for recipes (from a category) and filtering upon a selected category. #recipe

--- recipes/lib/ui/category_screen.dart
+++ recipes/lib/ui/category_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
+import '../models/recipe.dart';
+import '../models/dummy_data.dart';
 
 class CategoryScreen extends StatelessWidget {
   static const routePath = '/category';
@@ -8,12 +10,21 @@ class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final routeCategory = ModalRoute.of(context).settings.arguments as Category;
+    final filteredRecipes = DUMMY_RECIPES
+        .where((Recipe r) => r.categories.contains(routeCategory.id))
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Recipes: ${routeCategory.title}'),
       ),
       body: Center(
-        child: Text('Categories'),
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(filteredRecipes[index].title);
+          },
+          itemCount: filteredRecipes.length,
+        ),
       ),
     );
   }
