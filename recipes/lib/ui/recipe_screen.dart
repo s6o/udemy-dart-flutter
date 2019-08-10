94f0ee9dadc13531c435d4406cae9ab5d2a907fd
Oliver Sõro <oliver.s6ro@gmail.com>
2019-08-10 10:30:24 +0300

    Finish up Recipe screen #recipes

--- recipes/lib/ui/recipe_screen.dart
+++ recipes/lib/ui/recipe_screen.dart
@@ -15,7 +15,81 @@ class RecipeScreen extends StatelessWidget {
           overflow: TextOverflow.fade,
         ),
       ),
-      body: Center(child: Text(recipe.toString())),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                recipe.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            _buildSectionTitle(context, 'Ingredients'),
+            _buildItemContainer(
+              ListView.builder(
+                itemCount: recipe.ingredients.length,
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    color: Theme.of(context).accentColor,
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 5,
+                          horizontal: 10,
+                        ),
+                        child: Text(recipe.ingredients[index])),
+                  );
+                },
+              ),
+            ),
+            _buildSectionTitle(context, 'Steps'),
+            _buildItemContainer(
+              ListView.builder(
+                itemCount: recipe.steps.length,
+                itemBuilder: (ctx, index) {
+                  return Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('#${index + 1}'),
+                        ),
+                        title: Text(recipe.steps[index]),
+                      ),
+                      Divider(),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
+  Widget _buildItemContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      height: 250,
+      width: 300,
+      child: child,
     );
   }
 }
