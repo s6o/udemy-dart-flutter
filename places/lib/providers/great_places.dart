6b9935ce02d458f49cac539f998b09c32a38d899
Oliver Sõro <oliver.s6ro@gmail.com>
2019-09-01 15:23:16 +0300

    Integrate Db (insert, read) with GreatPlaces provider. #places

--- places/lib/providers/great_places.dart
+++ places/lib/providers/great_places.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 import '../models/place.dart';
+import '../models/db.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -12,14 +13,36 @@ class GreatPlaces with ChangeNotifier {
   }
 
   void addPlace(String title, File image) {
-    _items.add(
-      Place(
-        id: DateTime.now().toString(),
-        title: title,
-        image: image,
-        location: PlaceLocation(latitude: 0, longitude: 0),
-      ),
+    final item = Place(
+      id: DateTime.now().toString(),
+      title: title,
+      image: image,
+      location: PlaceLocation(latitude: 0, longitude: 0),
     );
+    _items.add(item);
+    notifyListeners();
+    Db.insert(
+      Table.Places,
+      {
+        'id': item.id,
+        'title': item.title,
+        'image': item.image.path,
+      },
+    );
+  }
+
+  Future<void> loadPlaces() async {
+    List<Map<String, dynamic>> places = await Db.query(Table.Places);
+    _items = places
+        .map(
+          (p) => Place(
+            id: p['id'],
+            title: p['title'],
+            image: File(p['image']),
+            location: PlaceLocation(latitude: 0, longitude: 0),
+          ),
+        )
+        .toList();
     notifyListeners();
   }
 }
